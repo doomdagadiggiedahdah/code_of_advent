0d8ab392fb40bb805cae8adf5992e27019473fed
mat <matdallen@gmail.com>
2023-12-06 01:43:04 -0800

    2015 day02 part 1&2

--- 2015/Day01.py
+++
@@ -1,8 +0,0 @@
-"""
-( = go up 1x
-) = go down 1x
-
-Talley the number, cross out, and give result.
-
-"""
-
---
+++ 2015/day02.py
@@ -0,0 +1,29 @@
+"""
+(2023.12.06__01.17.45) -- (2023.12.06__01.38.01) 
+
+wasn't 1715567
+sort didn't work on integers, needed to convert to ints first.
+"""
+
+input_text = open("./day02input.txt", "r").read().splitlines()
+wrapping_paper = 0
+ribbon = 0
+
+for present in input_text:
+    dimensions = present.split("x")
+
+    dimensions = [int(x) for x in dimensions]
+    dimensions.sort()
+    # print(dimensions)        
+
+    wrapping_paper += 2 * (dimensions[0] * dimensions[1])
+    wrapping_paper += 2 * (dimensions[0] * dimensions[2])
+    wrapping_paper += 2 * (dimensions[1] * dimensions[2])
+
+    ribbon += 2 * dimensions[0] + 2 * dimensions[1]
+    ribbon += dimensions[0] * dimensions[1] * dimensions[2]
+    
+    wrapping_paper += dimensions[0] * dimensions[1]
+
+print(wrapping_paper)
+print(ribbon)
