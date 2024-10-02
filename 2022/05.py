62df6c4c11389e478b3fa36601bcf303cfbc4c37
mat <matdallen@gmail.com>
2024-10-02 04:07:41 -0700

    05 pt2

--- 2022/05.py
+++ 2022/05.py
@@ -46,26 +46,24 @@ drawing, instructions = open('05input.txt', 'r').read().split("\n\n")
 lines = list(drawing.splitlines())
 lines = [list(i) for i in lines]
 matrix = np.rot90(lines, -1, (0,1))
-
 operate_on_me = [''.join(item).strip() for item in matrix if item[0].isdigit()] # this is close enough
-# numbers_only = [''.join(item) for item in matrix if item[0].isdigit()] # this is close enough
-# operate_on_me = [item[1:] for item in numbers_only] # finaly got it. but at what cost.
-
-# now what. needto process the instructions. jesus. over an hour for destructuring. maybe I needed it.
-# for i in operate_on_me:
-#     print(i)
-# print()
 
 instructions = instructions.splitlines()
 for line in instructions:
 
     num_move, from_col, to_col = [int(s) for s in line.split() if s.isdigit()] # ya boi is leraning
-    give_to = operate_on_me[to_col-1] ### give to
+    # give_to = operate_on_me[to_col-1] ### give to
 
-    for length in range(num_move):
-        take_me = operate_on_me[from_col-1][-1]
-        operate_on_me[to_col-1] += take_me
-        operate_on_me[from_col-1] = operate_on_me[from_col-1][:-1]
+    ## pt 1
+    # for length in range(num_move):
+    #     take_me = operate_on_me[from_col-1][-1]
+    #     operate_on_me[to_col-1] += take_me
+    #     operate_on_me[from_col-1] = operate_on_me[from_col-1][:-1]
+
+    ## pt 2
+    substring = operate_on_me[from_col-1][-num_move:]
+    operate_on_me[to_col-1] += substring
+    operate_on_me[from_col-1] = operate_on_me[from_col-1][:-len(substring)]
 
     # for i in operate_on_me:
     #     print(i)
@@ -81,3 +79,10 @@ print(''.join([item[-1][0] for item in operate_on_me]))
 # find the char, add to given, take from taken
 # just get the last digits of each 
 ### 03:57 holy shit dude. first try. yikes. that's cool. also, I'm tired.
+# ok, it's just the order that get's changed. 
+# before I was doing it iteratively; now I need to get the substring and remove it / add it accordingly.
+# same deeal of: get substring, remove then add.
+### 04:06; wow, solved that simply.
+# I may be wrong, but this seems like what you can get with good data structures?
+## if anything, I feel like I got the structure right, *some* destructuring, and then was able to move around it flexibly.
+# let's see what the others did.
